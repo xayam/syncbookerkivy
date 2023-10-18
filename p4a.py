a342acf1dfd77ecce59ee6bcf757ac5ff82bd7eb
xayam <xayam@yandex.ru>
2023-10-18 11:45:50 +0300

    v1.7 permissions moved to app.py

--- p4a.py
+++ p4a.py
@@ -1,6 +1,6 @@
 import subprocess
 
-VERSION = "1.6"
+VERSION = "1.7"
 
 ARCH = "armeabi-v7a"
 # ARCH = "x86_64"
--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from kivy import platform
+
 from src.model.model import Model
 from src.controller.controller import Controller
 from src.view.view import View
@@ -8,3 +10,17 @@ class App(Model, Controller, View):
         Model.__init__(self, app=self)
         Controller.__init__(self, app=self)
         View.__init__(self, app=self)
+
+        self.init()
+
+    @staticmethod
+    def init():
+        if platform == 'android':
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.INTERNET
+                                 # Permission.READ_EXTERNAL_STORAGE,
+                                 # Permission.WRITE_EXTERNAL_STORAGE,
+                                 # Permission.MEDIA_CONTENT_CONTROL,
+                                 # Permission.GLOBAL_SEARCH,
+                                 # Permission.READ_MEDIA_AUDIO
+                                 ])

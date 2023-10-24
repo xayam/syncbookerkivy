a58d5a01d7071294ff702d74c4758fde56aa0af4
xayam <xayam@yandex.ru>
2023-10-25 01:12:12 +0300

    v3.14 update adb.bat

--- adb.bat
+++ adb.bat
@@ -1,15 +1,15 @@
 @echo off
 
-powershell -Command "Invoke-WebRequest http://apk.delphima.z8.ru/apks/syncbooker.apk -OutFile syncbooker.apk"
+powershell -Command "Invoke-WebRequest http://apk.delphima.z8.ru/latest/latest.txt -OutFile latest.txt"
+set /p VERSION=<latest.txt
+set APK=syncbooker-armeabi-v7a-debug-%VERSION%.apk
+echo %APK%
+powershell -Command "Invoke-WebRequest http://apk.delphima.z8.ru/%VERSION%/%APK% -OutFile %APK%"
 
-adb.exe install -r syncbooker.apk
-
+adb.exe install -r %APK%
 adb.exe logcat --clear
-
 adb.exe shell monkey -p com.github.xayam.syncbookerkivy -c android.intent.category.LAUNCHER 1
-
 adb.exe -d logcat com.github.xayam.syncbookerkivy:V > logcat.txt
-
 notepad++.exe logcat.txt
 
 pause
--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export APP_VERSION="3.13"
+export APP_VERSION="3.14"
 export DEBUG=1
